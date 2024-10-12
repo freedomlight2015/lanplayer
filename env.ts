6e86107ff6f0e41402c385f00483fde652d2fea0
qinghe <2900762188@qq.com>
2024-10-12 14:51:07 +0800

    modify website title

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
-  SITE_NAME: 'LanPlayer',
+  SITE_NAME: 'LanPlayer - Local Network Video Sharing and Playback',
   OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  URL: 'https://www.lanplayer.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
